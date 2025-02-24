e5447663299f9d7f705f01361a34911215325f92
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-02-23 23:43:22 -0300

    Aula 5 Eventos e lógicas - Finalizado

--- Pasta Final/aluramidi-curso-aula5/main.js
+++ Pasta Final/aluramidi-curso-aula5/main.js
@@ -1,10 +1,12 @@
 function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
-    if(elemento && elemento.localName === 'audio') {
+    if (elemento && elemento.localName === 'audio') {
         elemento.play();
-    } else {
-        console.log("Elemento não encontrado");
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
     }
 
 }
@@ -19,17 +21,19 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
-        // tecla.classList.add('ativa');
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function(event) {
-        if (event.code === "Enter" || event.code === "Space") {
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
-        tecla.onkeyup = function() {
-            tecla.classList.remove('ativa');
-        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 
 }
